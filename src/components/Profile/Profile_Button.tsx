0fd25be6f574acce4c7af0ce6dcc3fd9d41235dd
Leyhara <vocalold@naver.com>
2024-06-28 15:59:38 +0900

    type 사용하기

--- src/components/Profile/Profile_Button.tsx
+++ src/components/Profile/Profile_Button.tsx
@@ -2,32 +2,32 @@ import Image from "next/image";
 import { FcAbout } from "react-icons/fc";
 
 type TProfile_Button_Type = {
-    isChecked: boolean;
-    src: string;
-    children: React.ReactNode;
+	isChecked: boolean;
+	src: string;
+	children: React.ReactNode;
 };
 
 export default function Profile_Button(props: TProfile_Button_Type) {
-    const { src, children, isChecked } = props;
-    return (
-        <>
-            <div
-                className={
-                    `flex w-[164px] h-[90px] items-center justify-center ` +
-                    `rounded-lg border ` +
-                    `text-[16px] ` +
-                    (isChecked &&
-                        `border-[#4171FF] font-bold text-[#4171FF] `) +
-                    (!isChecked &&
-                        `border-[#E3E3E3] font-normal text-[#676767] `)
-                }
-            >
-                <button className="flex flex-row gap-[10px]">
-                    {/* <Image src={src} alt="" /> {children} */}
-                    <FcAbout />
-                    {children}
-                </button>
-            </div>
-        </>
-    );
+	const { isChecked, src, children } = props;
+	return (
+		<>
+			<div
+				className={
+					`flex w-[164px] h-[90px] items-center justify-center ` +
+					`rounded-lg border ` +
+					`text-[16px] ` +
+					(isChecked &&
+						`border-[#4171FF] font-bold text-[#4171FF] `) +
+					(!isChecked &&
+						`border-[#E3E3E3] font-normal text-[#676767] `)
+				}
+			>
+				<button className="flex flex-row gap-[10px]">
+					{/* <Image src={src} alt="" /> {children} */}
+					<FcAbout />
+					{children}
+				</button>
+			</div>
+		</>
+	);
 }
