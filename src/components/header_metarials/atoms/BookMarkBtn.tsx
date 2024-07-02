f56ce5abd6757c70d0fafad711d2a12f58bfba01
Leyhara <vocalold@naver.com>
2024-07-02 14:25:41 +0900

    icons

--- src/components/header_metarials/atoms/BookMarkBtn.tsx
+++ src/components/header_metarials/atoms/BookMarkBtn.tsx
@@ -4,14 +4,14 @@ import { CiBookmark } from "react-icons/ci";
 import IconFormat from "../../common/atoms/IconFormat";
 
 function BookMarkBtn() {
-  return (
-    <IconFormat
-      icon={<CiBookmark />}
-      onClick={() => {
-        console.log("관심 목록이동 구현해주세요");
-      }}
-    />
-  );
+	return (
+		<IconFormat
+			icon={<CiBookmark />}
+			onClick={() => {
+				console.log("관심 목록이동 구현해주세요");
+			}}
+		/>
+	);
 }
 
 export default BookMarkBtn;
