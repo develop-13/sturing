92aba5806f524571bf830f28fc6ef93ef9048b89
Leyhara <vocalold@naver.com>
2024-07-02 13:36:37 +0900

    zx

--- src/types/user.ts
+++ src/types/user.ts
@@ -30,3 +30,17 @@ type TMood = string;
 type TCareer = "비기너" | "신입" | "주니어" | "시니어";
 type TLocation = string;
 type TStudyType = "온라인" | "오프라인" | "상관없음";
+
+/// Server Type
+type TUserServer = {
+	id: number;
+	name: string;
+	email: string;
+	profile: TUserProfile;
+	matching: TUserMatching;
+};
+
+/// Adapter To Server
+const adapterTUser = (user: TUser) => {
+	return;
+};
