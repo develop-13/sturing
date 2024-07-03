0cce359b97cdd242a9ffbe6033f635bc133051d2
jaesang <wotkd09093@gmail.com>
2024-07-04 01:05:47 +0900

    마이 프로필 헤더

--- src/app/user/me/edit/page.tsx
+++ src/app/user/me/edit/page.tsx
@@ -1,5 +1,16 @@
+import Icon from "@/components/common/atoms/Icon";
+import HeaderForamt from "@/components/header_metarials/organisms/headerForamt";
+
 function editMePage() {
-  return <div>editMePage</div>;
+  return (
+    <div>
+      <HeaderForamt
+        icons_left={<Icon type="BACK" />}
+        text_center="프로필 수정"
+        icons_right={"수정"}
+      />
+    </div>
+  );
 }
 
 export default editMePage;
