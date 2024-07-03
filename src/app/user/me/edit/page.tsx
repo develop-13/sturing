2d04a4e652bfd5127e7428cc45b1ad44c5defe2a
jaesang <wotkd09093@gmail.com>
2024-07-04 02:49:32 +0900

    마이페이지 수정 이미지부분

--- src/app/user/me/edit/page.tsx
+++ src/app/user/me/edit/page.tsx
@@ -1,7 +1,8 @@
+import CommonMarginDIv from "@/components/atoms/CommonMarginDIv";
 import Icon from "@/components/common/atoms/Icon";
 import HeaderForamt from "@/components/header_metarials/organisms/headerForamt";
 
-function editMePage() {
+function EditMePage() {
   return (
     <div>
       <HeaderForamt
@@ -9,8 +10,19 @@ function editMePage() {
         text_center="프로필 수정"
         icons_right={"수정"}
       />
+
+      {/* 이미지 섹션 */}
+      <div className="mt-6 mb-5 mx-auto w-[90px] h-[90px] bg-[url('/img/profileImg_example.png')] bg-cover rounded-full relative">
+        {/* 사진 */}
+        <div className="absolute bottom-0 right-0">
+          <Icon type="CAMERA" />
+        </div>
+      </div>
+      <CommonMarginDIv>
+        <h1 className="text-[18px] font-bold">기본정보</h1>
+      </CommonMarginDIv>
     </div>
   );
 }
 
-export default editMePage;
+export default EditMePage;
