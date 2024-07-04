b46a3a50ac6c80e5b3b568e76e5fca282960ffa1
Lumin <vocalold@naver.com>
2024-07-04 10:22:30 +0900

    types

--- src/components/pages/Home/HomePage.tsx
+++ src/components/pages/Home/HomePage.tsx
@@ -2,27 +2,32 @@ import StudyBox from "@/components/organisms/StudyBox";
 import StudyCategory from "@/components/organisms/StudyCategory";
 
 export default function HomePage() {
-	return (
-		<>
-			<div className="flex flex-col h-[2400px] mt-2 overflow-hidden">
-				<StudyCategory />
-				<div className="flex flex-row gap-2 mt-2">
-					<StudyBox
-						src=""
-						isChecked={false}
-						date="매주 금 오후 9:00"
-						type="오프라인"
-						category="개발·테크"
-					/>
-					<StudyBox
-						src=""
-						isChecked={false}
-						date="모임 날짜 미정"
-						type="온라인"
-						category="디자인"
-					/>
-				</div>
-			</div>
-		</>
-	);
+    const study1: TStudy = {
+        id: 0,
+        src: "",
+        date: "매주 금 오후 9:00",
+        type: "오프라인",
+        category: "개발·테크",
+        location: "",
+    };
+    const study2: TStudy = {
+        id: 1,
+        src: "",
+        date: "모임 날짜 미정",
+        type: "온라인",
+        category: "디자인",
+        location: "",
+    };
+
+    return (
+        <>
+            <div className="flex flex-col h-[2400px] mt-2 overflow-hidden">
+                <StudyCategory />
+                <div className="flex flex-row gap-2 mt-2">
+                    <StudyBox props={study1} />
+                    <StudyBox props={study2} />
+                </div>
+            </div>
+        </>
+    );
 }
