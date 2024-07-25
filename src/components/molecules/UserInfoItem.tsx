79352c624a125bd2bd19804425d4379646ca9ed3
jaesang <wotkd09093@gmail.com>
2024-07-25 09:19:28 +0900

    수정4

--- src/components/molecules/UserInfoItem.tsx
+++ src/components/molecules/UserInfoItem.tsx
@@ -17,14 +17,11 @@ const UserInfoItem = ({
 }: TUserInfoItem) => {
   return (
     <div className="flex items-center">
-      {/* <img
+      <img
         src={profileImage}
         alt={`${name} profile`}
         className="w-10 h-10 rounded-full mr-3"
-      /> */}
-      <div className="rounded-full mr-3">
-        <Image src={profileImage} width={40} height={40} />
-      </div>
+      />
       <div className="flex flex-col">
         <div className="flex items-center">
           <span className="font-bold">{name}</span>
