cdc9a9b6634f921d6e8e4f4ca7172639ed40f65e
jaesang <wotkd09093@gmail.com>
2024-11-29 13:42:14 +0900

    수정

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -5,21 +5,7 @@ import { TRoleText } from "@/types/common";
 import React from "react";
 
 function page() {
-  return (
-    <div>
-      <RoleSetterS
-        intro=""
-        handleSetRole={() => {}}
-        selectedRoles={["attendance_checker", "notification_leader"]}
-      />
-      <div className="h-5"> divider</div>
-      <RoleSetter
-        cancelRole={() => {}}
-        addRole={() => {}}
-        selectedRoles={new Set(["team_leader" as TRoleText])}
-      />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default page;
