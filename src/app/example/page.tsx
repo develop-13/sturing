4f3ef900f330298053e2185e3835ffe491c821d2
jaesang <wotkd09093@gmail.com>
2024-12-01 17:21:36 +0900

    example 페이지 정리

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -1,25 +1,9 @@
 "use client";
-import RoleSetter from "@/components/organisms/RoleSetter";
-import RoleSetterS from "@/components/organisms/recruitmentComponents/RoleSetter";
-import { TRoleText } from "@/types/common";
+
 import React from "react";
 
 function page() {
-  return (
-    <div>
-      <RoleSetterS
-        intro=""
-        handleSetRole={() => {}}
-        selectedRoles={["attendance_checker", "notification_leader"]}
-      />
-      <div className="h-5"> divider</div>
-      <RoleSetter
-        cancelRole={() => {}}
-        addRole={() => {}}
-        selectedRoles={new Set(["team_leader" as TRoleText])}
-      />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default page;
